c09d6fa9ec49aa8786e08c1fbc67bc8958e785ab
Vitaliy Yurchenko <world-rom@yandex.ru>
2021-11-26 08:29:28 +0400

    Добавление блока в index.html

---
+++ script.js
@@ -0,0 +1,12 @@
+//alert ('Hi, dude!');
+//console.log('Hi, console!');
+'use strict';
+let snake_Case = 10;
+const width = 11; //const переменная не поддается изменениям
+
+snake_Case = 30;
+
+console.log(snake_Case);
+
+
+
